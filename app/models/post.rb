77874ca450ff90b82cd761d9578ca21d454f2c75
Michael Ovakimian <mov.express17@gmail.com>
2022-12-19 23:39:55 +0300

    added posts model and display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  validates :header, presence: true
+  validates :content, presence: true
+  validates :author, presence: true
+  validates :heaader, length: { minimum: 5, maximum: 20 }
 end
