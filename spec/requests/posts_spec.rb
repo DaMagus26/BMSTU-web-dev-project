cd032e93200605880b051d408625d21429ed05dd
Michael Ovakimian <mov.express17@gmail.com>
2022-12-22 23:42:53 +0300

    final tweaks before JS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe "Posts", type: :request do
   describe "GET /create" do
     it "returns http success" do
       get "/posts/create"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET /delete" do
     it "returns http success" do
       get "/posts/delete"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
       get "/posts/new"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET /index" do
     it "returns http success" do
       get "/posts/index"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
